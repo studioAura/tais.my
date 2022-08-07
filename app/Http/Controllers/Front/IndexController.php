f8119259829fd2ec0b2151e0291d0176ddf76f68
Oleg <good.my.max@gmail.com>
2022-08-07 12:50:32 +0300

    start

---
+++ app/Http/Controllers/Front/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+//use App\Block;
+//use App\Direction;
+use App\Http\Controllers\Controller;
+//use App\Review;
+//use App\Slide;
+//use App\Work;
+//use Illuminate\Http\Request;
+//use TCG\Voyager\Models\Page;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+//        $slides = Slide::all();
+//        $home_1 = Block::where('position', 'home-1')->first();
+//        $home_2 = Block::where('position', 'home-2')->first();
+//        $directions = Direction::all();
+//        $works = Work::wherehas('folder')->where('is_favorite', '1')->get();
+//        $page = Page::where('id', '1')->first();
+//        $reviews = Review::orderBy('id', 'DESC')->take(5)->get();
+//
+//        return (view('pages.index', compact(
+//            'slides',
+//            'home_1',
+//            'home_2',
+//            'directions',
+//            'works',
+//            'page',
+//            'reviews'
+//        )));
+    }
+}
